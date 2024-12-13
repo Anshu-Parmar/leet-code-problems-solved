a548fc61767c710343776885dbe9f5776fa29014
Anshu Parmar <92868018+Anshu-Parmar@users.noreply.github.com>
2024-12-13 14:58:21 +0530

    Update LeetCode|Algorithm - Problem 83.dart

--- LeetCode|Algorithm - Problem 83.dart
+++ LeetCode|Algorithm - Problem 83.dart
@@ -1,16 +1,16 @@
-83. Remove Duplicates from Sorted List
+// 83. Remove Duplicates from Sorted List
 
-Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
-Input: head = [1,1,2,3,3]
-Output: [1,2,3]
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
  
-Constraints:
-	•	The number of nodes in the list is in the range [0, 300].
-	•	-100 <= Node.val <= 100
-	•	The list is guaranteed to be sorted in ascending order.
+// Constraints:
+// 	•	The number of nodes in the list is in the range [0, 300].
+// 	•	-100 <= Node.val <= 100
+// 	•	The list is guaranteed to be sorted in ascending order.
 
 
-SOLUTION:
+// SOLUTION:
 
 class Node<T> {
   T? value;
